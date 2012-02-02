2b5d1219f12b4465508e623910899fb33fdb54d8
icebird <manage@icebirds.net>
2012-02-02 11:21:15 +0800

    remove files don't need

--- docs/docsource/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curPath="/home/icebird/projects/webframework/release/docs/docsource"
-
-# 简体中文
-cd $curPath
-cd zh_cn
-xelatex -papersize=b5 -output-directory=/home/icebird/projects/webframework/release/docs/docsource/output hatalyst_analytics.tex
